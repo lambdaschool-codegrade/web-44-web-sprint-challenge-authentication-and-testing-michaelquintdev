2e0d05aee7233d1175efd129abcb2c432f000fd0
michaelquintdev <michaelquintdev@gmail.com>
2021-08-20 15:45:48 -0500

    Restriction is working

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,19 @@
+const jwt = require('jsonwebtoken')
+const {TOKEN_SECRET} = require('../../config/secret')
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+  const token = req.headers.authorization;
 
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  if(token) {
+    jwt.verify(token, TOKEN_SECRET, (err, decodedToken) => {
+      if(err) {
+        next({status:401, message: 'token invalid'})
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+  }else{
+    next({status: 400, message: 'token required'})
+  }
 };
